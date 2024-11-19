6a5213c52d0c581037a2a6d4a399a774746b20ef
prathapa <prathapa@golighthouse.com>
2024-11-19 03:09:40 -0800

    login window fixed

--- Views/UserNamePWDBoxs.xaml.cs
+++ Views/UserNamePWDBoxs.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Security;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace StayWise.Views
 {
@@ -7,9 +9,25 @@ namespace StayWise.Views
     /// </summary>
     public partial class UserNamePWDBoxs : UserControl
     {
+        public static readonly DependencyProperty PasswordProperty =
+                    DependencyProperty.Register("Password", typeof(SecureString), typeof(UserNamePWDBoxs));
+
+        public SecureString Password
+        {
+            get { return (SecureString)GetValue(PasswordProperty); }
+            set { SetValue(PasswordProperty, value); }
+        }
+
         public UserNamePWDBoxs()
         {
             InitializeComponent();
+            txtPassword.PasswordChanged += OnPasswordChanged;
+
+        }
+
+        private void OnPasswordChanged(object sender, RoutedEventArgs e)
+        {
+            Password = txtPassword.SecurePassword;
         }
     }
 }
